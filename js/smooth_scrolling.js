abefee1feef79bee24594b8e7afa41f231c7eeec
landkommunenhippie <hahn.tobias66@gmail.com>
2016-09-23 12:17:48 +0200

    added fixed navi and js to display it

--- js/smooth_scrolling.js
+++ js/smooth_scrolling.js
@@ -11,4 +11,17 @@ $(document).ready(function(){
       }
     }
   });
+
+  $(document).scroll(function(event){
+      //console.log($('#main-nav + div').position());
+      //console.log(event.pageY);
+      console.log(parseInt($('#main-nav').position().top));
+      if(parseInt($('#main-nav + div').position().top - event.pageY) <= 0){
+          $('#fixed-nav').show(800);
+      }else{
+        $('#fixed-nav').hide(1000);
+        
+      }
+    
+    });
 });
